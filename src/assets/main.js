84dd280feb93081b38db308a0647af26b643bbb6
Markus Mannila <mmannila@abo.fi>
2017-08-19 19:16:28 +0300

    Added necessary functions.

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,108 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    
+    // set new answer and reset number of attempts
+    if (answer.value == "" || attempt.value == "")
+    	setHiddenFields();
+    
+    // returns false if given input is invalid
+    if (!validateInput(input.value)) {
+    	return false;
+    }
+    attempt++;
+    
+    // updates message based on win/lose condition
+    if (getResults(input)) {
+    	setMessage("You Win! :)");
+    	showAnswer(true);
+    	showReplay();
+    }
+    else if (attempt >= 10) {
+    	setMessage("You Lose! :(");
+    	showAnswer(false);
+    	showReplay();
+    }
+    else
+    	setMessage("Incorrect! Try again.");
 }
 
-//implement new functions here
+function setHiddenFields() {
+	var num = Math.floor(Math.random() * 10000).toString();
+	num = "0000".substring(0, 4 - num.length) + num;
+	answer.value = num;
+	
+	attempt.value = 0;
+}
+
+function setMessage(msg) {
+	document.getElementById('message').innerHTML = msg;
+}
+
+function validateInput(input) {
+	if (input.length == 4)
+		return true;
+	else {
+		setMessage("Guesses must be exactly 4 characters long.");
+		return false;
+	}
+}
+
+function getResults(input) {
+	var newResult = document.createElement('div');
+	newResult.className = "row";
+	
+	var span = document.createElement('span');
+	span.className = "col-md-6";
+	span.innerHTML = input.value;
+	newResult.appendChild(span);
+	
+	var answers = document.createElement('div');
+	answers.className = "col-md-6";
+	
+	// keeps track on how much input is correct
+	var correct = 0;
+	
+	// shows how accurate each number is
+	for (i = 0; i < 4; i++) {
+		var res = document.createElement('span');
+		
+		if (input.value.charAt(i) === answer.value.charAt(i)) {
+			res.className = "glyphicon glyphicon-ok";
+			correct++;
+		}
+		else if (answer.value.indexOf(input.value.charAt(i)) >= 0) {
+			res.className = "glyphicon glyphicon-transfer";
+		}
+		else {
+			res.className = "glyphicon glyphicon-remove";
+		}
+		answers.appendChild(res);
+	}
+	
+	newResult.appendChild(answers);
+	
+	console.log(newResult);
+	document.getElementById('results').appendChild(newResult);
+	
+	if (correct == 4)
+		return true;
+	
+	return false;
+}
+
+function showAnswer(correct) {
+	var code = document.getElementById('code');
+	code.innerHTML = answer.value;
+	
+	if (correct)
+		code.className += " success";
+	else
+		code.className += " failure";
+}
+
+function showReplay() {
+	document.getElementById('guessing-div').style.display = "none";
+	document.getElementById('replay-div').style.display = "block";
+}
+
